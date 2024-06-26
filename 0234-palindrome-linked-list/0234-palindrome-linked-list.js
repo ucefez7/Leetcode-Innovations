0f1e4070d70d1900df880b44846d8af4aca70a1f
Yousafali EA <130924639+ucefez7@users.noreply.github.com>
2024-06-26 10:03:00 +0530

    Time: 170 ms (28.12%), Space: 81.5 MB (8.36%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.js
@@ -0,0 +1,19 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function(head) {
+    let arr=[]
+    while(head){
+        arr.push(head.val)
+        head= head.next
+    }
+    return arr.join('')==arr.reverse().join('')
+};

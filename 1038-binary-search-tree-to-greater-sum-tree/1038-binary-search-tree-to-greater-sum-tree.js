3fd01e8c68ed079589eb72137f5571bfba6daae4
Yousafali EA <130924639+ucefez7@users.noreply.github.com>
2024-07-05 18:48:55 +0530

    Time: 52 ms (62.88%), Space: 48.8 MB (84.67%) - LeetHub

---
+++ 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var bstToGst = function(root) {
+    let res=0
+    inorder=(root)=>{
+        if(!root)return
+        inorder(root.right)
+        res= res+ root.val
+        root.val= res
+        inorder(root.left)
+    }
+    inorder(root)
+    return root
+};

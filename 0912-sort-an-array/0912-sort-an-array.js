a479f0934931380a709072aae74de67d19579d35
Yousafali EA <130924639+ucefez7@users.noreply.github.com>
2024-07-26 09:34:15 +0530

    Time: 1075 ms (11.98%), Space: 73.5 MB (45.32%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.js
+++ 0912-sort-an-array/0912-sort-an-array.js
@@ -3,5 +3,23 @@
  * @return {number[]}
  */
 var sortArray = function(nums) {
-    return nums.sort((a,b)=>a-b)
+    if(nums.length<2){
+        return nums
+    }
+    let mid= Math.floor(nums.length/2)
+    let left= nums.slice(0,mid)
+    let right= nums.slice(mid)
+    return merge(sortArray(left),sortArray(right))
 };
+
+merge=(left,right)=>{
+    arr=[]
+    while(left.length && right.length){
+        if(left[0]<right[0]){
+            arr.push(left.shift())
+        }else{
+            arr.push(right.shift())
+        }
+    }
+    return [...arr,...left,...right]
+}
